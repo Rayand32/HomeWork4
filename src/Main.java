2ae16833c0626719c1a2a74c95e7c29e4a179b03
Rayand <nopechan2020@gmail.com>
2022-04-15 17:43:51 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        int squareSite = 4;
+        int squareNumberSite = 4;
+        int result;
+
+        System.out.println("Программа позволяет узнать периметр квадрата");
+
+        result = squareSite * squareNumberSite;
+
+        System.out.print(" Периметр квадрата составляет: " + result + " см.");
+
+
+
+
+
+
+
+
+    }
+}
